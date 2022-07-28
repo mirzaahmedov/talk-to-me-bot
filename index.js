6268cdb25d2658741ab79185e595f80b3002eeab
mirzaahmedov.dev <mirzaahmedov.dev@gmail.com>
2022-07-28 11:09:56 +0500

    finished

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-import dotenv from "dotenv"
-import express from "express"
-import http from "http"
-import cors from "cors"
-import TelegramBot from "node-telegram-bot-api"
-import { Server } from "socket.io"
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import TelegramBot from "node-telegram-bot-api";
+import { Server } from "socket.io";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT
-const BOT_TOKEN = process.env.BOT_TOKEN
+const PORT = process.env.PORT;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server, {  cors: { origin: "*" } })
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-app.use(cors())
-app.use(express.static("public"))
+app.use(cors());
+app.use(express.static("public"));
 
-app.get("/", function(req, res){
-	res.sendFile('public/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile("public/index.html");
+});
 
 server.listen(PORT, () => {
-	console.log(`server up and running on PORT ${PORT}`)
+  console.log(`server up and running on PORT ${PORT}`);
 
-	const bot = new TelegramBot(BOT_TOKEN,{ polling: true })
+  const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-	bot.on("message", async function(msg){
-		const { type } = msg?.chat
-		const { id } = msg.from
+  bot.on("message", async function (msg) {
+    const { type } = msg?.chat;
+    const { id } = msg.from;
 
-		if (type === "private") {
-			const { total_count, photos } = await bot.getUserProfilePhotos(id)
-		}
+    if (type === "private") {
+      const { total_count, photos } = await bot.getUserProfilePhotos(id);
+      console.log(total_count);
+    }
 
-		io.emit("new_message", { query: { ok: true } })
-	})
+    io.emit("new_message", {
+      text: msg.text,
+      fromId: msg.chat.id,
+      from: `${msg.from.last_name} ${msg.from.first_name}`,
+    });
+  });
 
-	io.on('connection', (socket) => {
-		console.log('a user connected', socket.query);
-	});
-})
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("response", (socket) => {
+      bot.sendMessage(socket.to, socket.value);
+    });
+  });
+});
--- public/src/Chat.jsx
+++ public/src/Chat.jsx
@@ -1,21 +1,46 @@
-import React from "react"
-import { io } from "socket.io-client"
+import React from "react";
+import { io } from "socket.io-client";
 
-export default function Chat(){
+export default function Chat() {
+  const socket = React.useRef(io("ws://localhost:4000"));
 
-	React.useEffect(function(){
-		const socket = io("ws://localhost:4000", {
-			query: { some: "Some text" }
-		})
+  const [value, setValue] = React.useState("");
+  const [messages, setMessages] = React.useState([]);
+  const [activeUser, setActiveUser] = React.useState(null);
 
-		socket.on("new_message", function (socket){
-			console.log("new msg")
-		})
-	},[])
+  React.useEffect(function () {
+    socket.current.on("new_message", function (socket) {
+      setMessages((state) => [
+        ...state,
+        { text: socket.text, from: socket.from },
+      ]);
+      setActiveUser(socket.fromId);
+    });
+  }, []);
 
-	return (
-		<div>
-			Hello Chat
-		</div>
-	)
+  return (
+    <div>
+      {messages.length > 0 &&
+        messages.map((message) => (
+          <div>
+            <b>{message.from}</b>
+            {message.text}
+          </div>
+        ))}
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button
+          onClick={() => {
+            socket.current.emit("response", { value: value, to: activeUser });
+          }}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
 }
